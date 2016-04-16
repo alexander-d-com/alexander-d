49167465a1fa48a61ddc6eb5f3bae894ae65c209
Alexander <alessandro.diez@email.com>
2016-04-16 18:55:40 +0200

    divided page into worpress core files

---
+++ wp-theme/alexander-d/footer.php
@@ -0,0 +1,6 @@
+      <footer>
+        <div class="footer-text">© 2016 alexander-d.com</div>
+      </footer>
+    </div>
+  </body>
+</html>
---
+++ wp-theme/alexander-d/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head <?php language_attributes(); ?>>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Alexander D</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body <?php body_class(); ?>>
+    <div class="container">
+      <header>
+        <div class="title">Alexander</div>
+        <div class="sub-title">Unluckly, my career is online.</div>
+      </header>
